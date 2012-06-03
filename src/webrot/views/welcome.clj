8341a8121c09127a8cdc17c3fb40fbee30ea290a
Richard Hull <rm_hull@yahoo.co.uk>
2012-06-03 23:49:18 +0100

    Temporary comment out hiccup - not compiling in clojure 1.4.0

--- src/webrot/views/welcome.clj
+++ src/webrot/views/welcome.clj
@@ -2,13 +2,14 @@
   (:require [webrot.views.common :as common]
             [webrot.models.lut :as lut])
   (:use [noir.core :only [defpage custom-handler]]
-        [hiccup.core :only [html]])
+ ;       [hiccup.core :only [html]]
+        [webrot.models.fractal :only [fractal mandlebrot-set julia-set]])
   (:import [java.awt.image BufferedImage]))
 
-(defpage "/welcome" []
-         (common/layout
-           [:h1 "Welcome to webrot!"]
-           [:p "You are visitor zero."]))
+;(defpage "/welcome" []
+;         (common/layout
+;[:h1 "Welcome to webrot!"]
+;           [:p "You are visitor zero."]))
 
 (defpage "/test/:lut" {:keys [lut]}
   (let [w 800
@@ -19,3 +20,17 @@
             y (range h)]
       (.setRGB img x y (lut/get-color colors y)))
     img))
+
+(defpage "/mandlebrot/:lut" {:keys [lut]}
+  (let [w 800
+        h 600
+        color-map (partial lut/get-color (lut/from-name lut))]
+    (fractal [w h] (mandlebrot-set) 50 color-map)))
+
+(defpage "/julia/:lut" {:keys [lut]}
+  (let [w 800
+        h 600
+        color-map (partial lut/get-color (lut/from-name lut))]
+    (fractal [w h] (julia-set [-1.0 0.0001]) 50 color-map)))
+
+
