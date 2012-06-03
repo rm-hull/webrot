3fd64f520e94796d32472b63587830bd3193ba24
Richard Hull <rm_hull@yahoo.co.uk>
2012-06-03 23:46:22 +0100

    Use latest version of Noir

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject webrot "0.1.0-SNAPSHOT"
             :description "A web-based Mandlebrot-set generator"
             :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.2.1"]]
+                           [noir "1.3.0-beta7"]]
             :main webrot.server)
 
