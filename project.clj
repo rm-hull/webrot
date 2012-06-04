8c1f824d306d68b3aaf4f177238c7a8ddfdf068b
Richard Hull <rm_hull@yahoo.co.uk>
2012-06-04 23:29:26 +0100

    Added clojurescript plugin

--- project.clj
+++ project.clj
@@ -1,6 +1,17 @@
 (defproject webrot "0.1.0-SNAPSHOT"
-            :description "A web-based Mandlebrot-set generator"
-            :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.3.0-beta7"]]
-            :main webrot.server)
+  :description "A web-based Mandlebrot-set generator"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [noir "1.3.0-beta7"]]
+  :main webrot.server
+  :plugins [[lein-cljsbuild "0.2.1"]]
+  :cljsbuild {
+    :builds [{
+        ; The path to the top-level ClojureScript source directory:
+        :source-path "src-cljs"
+        ; The standard ClojureScript compiler options:
+        ; (See the ClojureScript compiler documentation for details.)
+        :compiler {
+          :output-to "war/javascripts/main.js"  ; default: main.js in current directory
+          :optimizations :whitespace
+          :pretty-print true}}]})
 
