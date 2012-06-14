e7a56ad31539a0185e4745617b764f7e4be4c8b1
Richard Hull <rm_hull@yahoo.co.uk>
2012-06-14 23:41:16 +0100

    Refactor to use byte array input/output streams for the pipe... piped methods slower & occasionally throw broken pipe

--- src/webrot/views/common.clj
+++ src/webrot/views/common.clj
@@ -6,7 +6,7 @@
         [clojure.java.io :only [make-input-stream]])
   (:import [javax.imageio ImageIO]
            [java.awt.image RenderedImage]
-           [java.io PipedInputStream PipedOutputStream]))
+           [java.io ByteArrayInputStream ByteArrayOutputStream]))
 
 (defpartial layout [& content]
   (html5
@@ -18,15 +18,13 @@
         content]]))
 
 (defn- create-pipe [f pipe-size]
-  (let [in-stream (PipedInputStream. pipe-size)]
-    (future
-      (with-open [out-stream (PipedOutputStream. in-stream)]
-        (f out-stream)))
-    in-stream))
+  (with-open [out-stream (ByteArrayOutputStream. pipe-size)]
+    (f out-stream)
+    (ByteArrayInputStream. (.toByteArray out-stream))))
 
 (extend-protocol Renderable
   RenderedImage
   (render [this _]
-    (let [stream (create-pipe #(ImageIO/write this "png" %) 0x10000)]
+    (let [stream (create-pipe #(ImageIO/write this "png" %) 0x20000)]
       (content-type (response stream) "image/png"))))
 
