9a7a9b0a5acdb2e83b9f3522cbf57b9145a19068
Richard Hull <rm_hull@yahoo.co.uk>
2012-07-01 10:06:07 +0100

    Added no deps to prevent 404 on non-existent deps.js

--- src/webrot/views/common.clj
+++ src/webrot/views/common.clj
@@ -2,12 +2,25 @@
   (:use [noir.core :only [defpartial]]
         [hiccup.core :only [html]]
         [hiccup.page :only [include-css include-js html5]]
+        [hiccup.element :only [javascript-tag]]
         [compojure.response]
         [ring.util.response :only [response content-type]]
         [clojure.java.io :only [make-input-stream]])
   (:import [javax.imageio ImageIO]
            [java.awt.image RenderedImage]
            [java.io ByteArrayInputStream ByteArrayOutputStream]))
+
+; When using {:optimizations :whitespace}, the Google Closure compiler combines
+; its JavaScript inputs into a single file, which obviates the need for a "deps.js"
+; file for dependencies.  However, true to ":whitespace", the compiler does not remove
+; the code that tries to fetch the (nonexistent) "deps.js" file.  Thus, we have to turn
+; off that feature here by setting CLOSURE_NO_DEPS.
+;
+; Note that this would not be necessary for :simple or :advanced optimizations.
+(defn- include-clojurescript [path]
+  (list
+      (javascript-tag "var CLOSURE_NO_DEPS = true;")
+          (include-js path)))
 
 (defpartial layout [& content]
   (html5
@@ -20,7 +33,7 @@
     [:body
      [:div#wrapper
       content]
-      (include-js "/cljs/bootstrap.js")
+      (include-clojurescript "/cljs/bootstrap.js")
      ]))
 
 (defpartial spinner [css-class]
