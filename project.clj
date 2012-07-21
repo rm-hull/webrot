2189d41c86c05bb14793c3411b552c2baa849b26
Richard Hull <rm_hull@yahoo.co.uk>
2012-07-21 23:23:51 +0100

    Migrate to leiningen 2

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject webrot "0.1.0-SNAPSHOT"
-  :description "A web-based Mandlebrot-set generator"
+(defproject webrot/webrot "0.1.0-SNAPSHOT" 
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta10"]
                  [jayq "0.1.0-alpha4"]
@@ -9,17 +8,17 @@
                  [ring-basic-authentication "1.0.1"]
                  [ibdknox/ring-gzip-middleware "0.1.1"]
                  [com.cemerick/drawbridge "0.0.6"]]
+  :cljsbuild {:builds
+              [{:source-path "src",
+                :compiler
+                {:output-to "resources/public/cljs/bootstrap.js",
+                 :optimizations :whitespace,
+                 :externs
+                 ["https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"],
+                 :pretty-print true}}]}
+  :profiles {:dev {:dependencies [[vimclojure/server "2.3.3"]]}}
   :main webrot.server
+  :min-lein-version "2.0.0"
   :warn-on-reflection true
   :plugins [[lein-cljsbuild "0.2.1"]]
-  :dev-dependencies [[vimclojure/server "2.3.3"]]
-  ;:hooks [leiningen.cljsbuild]
-  :cljsbuild {
-    :builds [{
-        :source-path "src"
-        :compiler {
-          :output-to "resources/public/cljs/bootstrap.js"
-          :optimizations :whitespace ;:advanced
-          :externs ["https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"]         
-          :pretty-print true}}]})
-
+  :description "A web-based Mandlebrot-set generator")
