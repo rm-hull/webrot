26875022a430826ad8f376de19cefaa876b77983
Richard Hull <rm_hull@yahoo.co.uk>
2012-09-16 16:51:44 +0100

    First cut

--- src/webrot/data_mappers/cellular_automata.clj
+++ src/webrot/data_mappers/cellular_automata.clj
@@ -1,45 +1,92 @@
-(ns webrot.data-mappers.bounds)
+(ns webrot.data-mappers.cellular-automata)
 
-(defrecord Bounds [top right bottom left])
+(def glider 
+  #{[2 0] [2 1] [2 2] [1 2] [0 1]})
 
-(defn to-bounds [[top rgt bot lft]]
-  (Bounds. 
-    (max top bot)
-    (max lft rgt)
-    (min top bot)
-    (min lft rgt)))
+(def light-spaceship 
+  #{[2 0] [4 0] [1 1] [1 2] [1 3] [4 3] [1 4] [2 4] [3 4]})
+ 
+(def oscillator
+  #{[1 0] [1 1] [1 2]})
 
-(defn- abs [n]
-  (if (neg? n) (- n) n))
+(def neighbours
+  (for [i [-1 0 1]
+        j [-1 0 1]
+        :when (not= 0 i j)]
+    [i j]))
 
-(defn width [bounds]
-  (abs (- (:left bounds) (:right bounds))))
+(defn transform 
+  "Transforms a point [x y] by a given offset [dx dy]"
+  [[x y] [dx dy]]
+  [(+ x dx) (+ y dy)])
 
-(defn height [bounds]
-  (abs (- (:top bounds) (:bottom bounds))))
+(defn place [artefact position]
+  (map (partial transform position) artefact))
 
-(defn zoom-in [bounds screen x y]
-  "Recalculate bounds (zoom in by 50%)"
-  (let [bot (+ (:bottom bounds) (* y (/ (height bounds) (height screen))) (/ (height bounds) -4))
-        lft (+ (:left bounds)   (* x (/ (width bounds)  (width screen)))  (/ (width bounds) -4))]
-    (Bounds.
-      (double (+ bot (/ (height bounds) 2))) ; top
-      (double (+ lft (/ (width bounds) 2)))  ; right
-      (double bot)
-      (double lft))))
+(defn random-world 
+  "Makes a random collection of cells of the given dimensions and population count"
+  [[w h] items]
+  (into #{}
+    (take items (map vector 
+                     (repeatedly #(int (rand w)))
+                     (repeatedly #(int (rand h)))))))
 
-(defn zoom-out [bounds screen x y]
-  "Recalculate bounds (zoom out by 50%)"
-  (let [bot (+ (:bottom bounds) (* y (/ (height bounds) (height screen))) (- (height bounds)))
-        lft (+ (:left bounds)   (* x (/ (width bounds)  (width screen)))  (- (width bounds)))]
-    (Bounds.
-      (double (+ bot (* (height bounds) 2))) ; top
-      (double (+ lft (* (width bounds) 2)))  ; right
-      (double bot)
-      (double lft))))
+(def world
+  (into #{}
+    (concat
+      (place oscillator [25 5])
+      (place oscillator [17 2])
+      (place glider [7 3])
+      (place glider [10 11])
+      (place glider [34 7])
+      (place light-spaceship [4 16]))))
+ 
+(defn maxf [f coll]
+  (reduce max (map f coll)))
 
-(defn real-coords [bounds screen x y]
-  (let [bounds (to-bounds bounds)
-        screen (to-bounds screen)]
-  { :x (double (+ (:left bounds)   (* (width bounds)  (/ x (width screen)))))
-    :y (double (+ (:bottom bounds) (* (height bounds) (/ y (height screen))))) }))
+(defn to-grid
+  ([cells] (to-grid cells [(maxf first cells) (maxf second cells)]))
+  ([cells [w h]]
+    (for [y (range (inc h))]
+      (for [x (range (inc w))
+            :let [loc [x y]]]
+        (if (cells loc) "X" " ")))))
+     
+(defn display [grid-rows]
+  (doall (map #(println "[" (apply str %) "]") grid-rows))
+  (println))
+  
+(defn stepper [neighbours birth? survive?]
+  (fn [cells]
+    (set (for [[loc n] (frequencies (mapcat neighbours cells))
+               :when (if (cells loc) (survive? n) (birth? n))]
+           loc))))
+
+(def conways-game-of-life
+  (stepper (partial place neighbours) #{3} #{2 3}))
+
+(def generations (partial iterate conways-game-of-life))
+
+;(def x (random-world [40 30] 200))
+;
+;(display (nth (map to-grid (generations x)) 0))
+;
+;(display (nth (map to-grid (generations x)) 1))
+;
+;(display (nth (map to-grid (generations x)) 2))
+;
+;(display (nth (map to-grid (generations x)) 3))
+;
+;(display (nth (map to-grid (generations x)) 4))
+;
+;(display (nth (map to-grid (generations x)) 5))
+;
+;(display (nth (map to-grid (generations x)) 6))
+;
+;(display (nth (map to-grid (generations x)) 7))
+;
+;(display (nth (map to-grid (generations x)) 8))
+;
+;(display (nth (map to-grid (generations x)) 9))
+;
+;(display (nth (map to-grid (generations x)) 10))
