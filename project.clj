c6eaa6c7f486a95d5212a39c0bb8cb5d5e13609d
Richard Hull <rm_hull@yahoo.co.uk>
2012-09-17 00:38:28 +0100

    Use custom monet implementation

--- project.clj
+++ project.clj
@@ -4,23 +4,29 @@
                  [jayq "0.1.0-alpha4"]
                  [crate "0.2.1"]
                  [fetch "0.1.0-alpha2"]
-                 [monet "0.1.0-SNAPSHOT"]
+                 [org.clojars.rm-hull/monet "0.1.1-SNAPSHOT"]
                  [ring/ring-core "1.1.5"]
                  [ring-basic-authentication "1.0.1"]
                  [ibdknox/ring-gzip-middleware "0.1.1"]
                  [com.cemerick/drawbridge "0.0.6"]]
-  :cljsbuild {:builds
-              [{:source-path "src",
-                :compiler
-                {:output-to "resources/public/cljs/bootstrap.js",
-                 :optimizations :whitespace,
-                 :externs
-                 ["https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"],
-                 :pretty-print true}}]}
+  :cljsbuild {
+    :builds [
+      {:source-path "src/webrot/client/mandlebrot",
+       :compiler {:output-to "resources/public/cljs/mandlebrot.js"
+                  :optimizations :whitespace
+                  :externs ["https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"]
+                  :pretty-print true}}
+      {:source-path "src/webrot/client/conway",
+       :compiler {:output-to "resources/public/cljs/conway.js"
+                  :optimizations :whitespace
+                  :externs ["https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"]
+                  :pretty-print true}}
+             
+    ]}
   :hooks [leiningen.cljsbuild]
   :plugins [[lein-cljsbuild "0.2.7"]]
   :profiles {:dev {:dependencies [[vimclojure/server "2.3.6"]]}}
   :main webrot.server
   :min-lein-version "2.0.0"
   :warn-on-reflection true
-  :description "A web-based Mandlebrot-set generator")
+  :description "A web-based fractal generator")
