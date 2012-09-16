5c7fe60985ea0bfa752de669c96b8fc969e351c2
Richard Hull <rm_hull@yahoo.co.uk>
2012-09-17 00:38:07 +0100

    1st cut; draws random world

--- src/webrot/client/conway/core.cljs
+++ src/webrot/client/conway/core.cljs
@@ -4,8 +4,10 @@
 
 (def ctx (get-context (.get ($ :#conway) 0) "2d"))
 
-(defn dot [ctx [x y]]
-  (circle ctx {:x (* x 4) :y (* y 4) :r 4}))
+(defn dot 
+  ([ctx [x y]] (dot ctx [x y] 4))
+  ([ctx [x y] size]
+    (circle ctx {:x (+ size (* x size 2)) :y (+ size (* y size 2)) :r size})))
 
 (def glider 
   #{[2 0] [2 1] [2 2] [1 2] [0 1]})
@@ -15,7 +17,6 @@
  
 (def oscillator
   #{[1 0] [1 1] [1 2]})
-
 
 (defn transform 
   "Transforms a point [x y] by a given offset [dx dy]"
@@ -35,12 +36,19 @@
       (place glider [34 7])
       (place light-spaceship [4 16]))))
 
+(defn random-world 
+  "Makes a random collection of cells of the given dimensions and population count"
+  [[w h] items]
+  (into #{}
+    (take items (map vector 
+                     (repeatedly #(int (rand w)))
+                     (repeatedly #(int (rand h)))))))
 
 (defn maxf [f coll]
   (reduce max (map f coll)))
 
-(defn draw-world
-  ([ctx cells] (draw-world ctx cells [(maxf first cells) (maxf second cells)]))
+(defn draw-cells
+  ([ctx cells] (draw-cells ctx cells [(maxf first cells) (maxf second cells)]))
   ([ctx cells [w h]]
     (for [y (range (inc h))
           x (range (inc w))
@@ -48,19 +56,18 @@
                 [color opacity] (if (cells locn) 
                                   ["#ffff00" 1.0] 
                                   ["#000000" 0.5])]]
-      (-> ctx (fill-style color) (alpha opacity) (dot locn)))))
-          
+      (-> ctx 
+          (fill-style color) 
+          (alpha opacity) 
+          (dot locn)))
+;    ctx))
+  ))
+
 (document-ready
   (fn []
     (-> ctx
-        (draw-world [50 50] world)
-        ;(fill-style "#ffff00")
-        ;(circle {:x 20 :y 26 :r 4})
-        ;(circle {:x 28 :y 26 :r 4})
-        ;(circle {:x 28 :y 34 :r 4})
-        ;(alpha 0.5)
-        ;(fill-style "#ff0000")
-        ;(rect {:x 50 :y 25 :w 150 :h 100})
+        (draw-cells (random-world [50 50] 200) [50 50])
+        (dot [2 0])
       )))
 
 
