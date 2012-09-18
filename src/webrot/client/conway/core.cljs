050ef4a8f070d2dd8539306b20bb2b791dd0718a
Richard Hull <rm_hull@yahoo.co.uk>
2012-09-18 22:26:01 +0100

    Refactor conway

--- src/webrot/client/conway/core.cljs
+++ src/webrot/client/conway/core.cljs
@@ -7,6 +7,12 @@
 (def glider 
   #{[2 0] [2 1] [2 2] [1 2] [0 1]})
 
+(def beacon 
+  #{[0 0] [0 1] [1 0] [1 1] [2 2] [2 3] [3 2] [3 3]})
+
+(def toad
+  #{[2 3] [2 4] [3 5] [4 2] [5 3] [5 4]})
+
 (def light-spaceship 
   #{[2 0] [4 0] [1 1] [1 2] [1 3] [4 3] [1 4] [2 4] [3 4]})
  
@@ -26,27 +32,22 @@
 
 (defn place [artefact position]
   (map (partial transform position) artefact))
- 
-(defn stepper [neighbours birth? survive?]
-  (fn [cells]
-    (set (for [[loc n] (frequencies (mapcat neighbours cells))
-               :when (if (cells loc) (survive? n) (birth? n))]
-           loc))))
-
-(def conways-game-of-life
-  (stepper (partial place neighbours) #{3} #{2 3}))
 
 (defn dot [ctx [x y]]
-  (rect ctx {:x (* x 8) :y (* y 8) :w 7 :h 7}))
+  (rect ctx {:x (bit-shift-left x 3) :y (bit-shift-left y 3) :w 7 :h 7}))
 
 (def sample-world
   (into #{}
     (concat
       (place oscillator [25 5])
       (place oscillator [17 2])
-      (place glider [7 3])
+      (place glider [57 3])
       (place glider [10 11])
       (place glider [34 7])
+      (place toad [90 7])
+      (place beacon [90 27])
+      (place light-spaceship [72 19])
+      (place light-spaceship [12 6])
       (place light-spaceship [4 16]))))
 
 (defn random-world 
@@ -58,12 +59,27 @@
                      (repeatedly #(int (rand h)))))))
 
 (def size [100 75])
-(def worlds (atom (random-world size 800)))
-;(def worlds (atom sample-world))
+(def world (atom (random-world size 800)))
+;(def world (atom sample-world))
 
-(defn sanitize [world [w h]]
-  (let [pred (fn [[x y]] (and (>= x 0) (>= y 0) (< x w) (< y h)))]
-    (set (distinct (filter pred world)))))
+(defn sanitize [[x y]]
+  (and 
+    (>= x 0)
+    (>= y 0)
+    (< x (size 0))
+    (< y (size 1))))
+
+ 
+(defn stepper [neighbours birth? survive? check-fn]
+  (fn [cells]
+    (set (for [[loc n] (frequencies (mapcat neighbours cells))
+               :when (and
+                       (if (cells loc) (survive? n) (birth? n))
+                       (check-fn loc))]
+           loc))))
+
+(def conways-game-of-life
+  (stepper (partial place neighbours) #{3} #{2 3} sanitize))
 
 (defn draw-cells [ctx cells [w h]]
   (-> ctx
@@ -78,9 +94,9 @@
 
 (defn animate []
   (. js/window (requestAnimFrame animate))
-  (let [current (deref worlds)]
+  (let [current (deref world)]
     (draw-cells ctx current size)
-    (swap! worlds [] (sanitize (conways-game-of-life current) size))))
+    (swap! world conways-game-of-life current)))
      
 (animate)
 
