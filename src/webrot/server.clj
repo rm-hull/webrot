0de1d09f560b09213a37d97f8aefd594109588d5
Richard Hull <rm_hull@yahoo.co.uk>
2012-12-23 20:45:18 +0000

    Fix reflection warning

--- src/webrot/server.clj
+++ src/webrot/server.clj
@@ -1,36 +1,36 @@
 (ns webrot.server
   (:require [noir.server :as server]
             [noir.fetch.remotes :as remotes]
-            [cemerick.drawbridge :as drawbridge]
-            [ring.middleware.params :as params]
-            [ring.middleware.keyword-params :as keyword-params]
-            [ring.middleware.nested-params :as nested-params]
-            [ring.middleware.session :as session]
-            [ring.middleware.basic-authentication :as basic]
+;            [cemerick.drawbridge :as drawbridge]
+;            [ring.middleware.params :as params]
+;            [ring.middleware.keyword-params :as keyword-params]
+;            [ring.middleware.nested-params :as nested-params]
+;            [ring.middleware.session :as session]
+;            [ring.middleware.basic-authentication :as basic]
             [ring.middleware.gzip :as deflate]))
 
 (server/load-views "src/webrot/views/")
 
-(def drawbridge-handler
-  (-> (drawbridge/ring-handler)
-      (keyword-params/wrap-keyword-params)
-      (nested-params/wrap-nested-params)
-      (params/wrap-params)
-      (session/wrap-session)))
+;(def drawbridge-handler
+;  (-> (drawbridge/ring-handler)
+;      (keyword-params/wrap-keyword-params)
+;      (nested-params/wrap-nested-params)
+;      (params/wrap-params)
+;      (session/wrap-session)))
 
 (defn authenticated? [name pass]
   (= [name pass] [(System/getenv "AUTH_USER") (System/getenv "AUTH_PASS")]))
 
-(defn wrap-drawbridge [handler]
-  (fn [req]
-    (let [handler (if (= "/repl" (:uri req))
-                    (basic/wrap-basic-authentication drawbridge-handler authenticated?)
-                    handler)]
-      (handler req))))
+;(defn wrap-drawbridge [handler]
+;  (fn [req]
+;    (let [handler (if (= "/repl" (:uri req))
+;                    (basic/wrap-basic-authentication drawbridge-handler authenticated?)
+;                    handler)]
+;      (handler req))))
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "8080"))]
+        port (read-string (get (System/getenv) "PORT" "8080"))]
     ;(server/add-middleware wrap-drawbridge)
     (server/add-middleware deflate/wrap-gzip)
     (server/start port {:mode mode
